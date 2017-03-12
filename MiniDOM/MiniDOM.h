4a6193edba07d4fb37640d3c84c4eb048befd218
Paul Calnan <me@paulcalnan.com>
2017-03-12 11:07:44 -0400

    Initial Commit

---
+++ MiniDOM/MiniDOM.h
@@ -0,0 +1,19 @@
+//
+//  MiniDOM.h
+//  MiniDOM
+//
+//  Created by Paul Calnan on 3/12/17.
+//  Copyright © 2017 Anodized Software, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MiniDOM.
+FOUNDATION_EXPORT double MiniDOMVersionNumber;
+
+//! Project version string for MiniDOM.
+FOUNDATION_EXPORT const unsigned char MiniDOMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniDOM/PublicHeader.h>
+
+
