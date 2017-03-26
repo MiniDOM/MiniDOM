071341e211722d3ed4af0b558c064da081c175b7
Paul Calnan <me@paulcalnan.com>
2017-03-25 21:59:08 -0400

    Removed documentation from main repo

--- bin/GenerateDocumentation.sh
+++ bin/GenerateDocumentation.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 project_root="$( cd "$script_dir" && cd .. && pwd )"
-output_dir="$project_root/Documentation"
+output_dir="$project_root/../MiniDOM.github.io"
 
 jazzy \
     --clean \
     --author "MiniDOM" \
-    --author_url "https://github.com/MiniDOM/MiniDOM" \
+    --author_url "https://minidom.github.io" \
     --github_url "https://github.com/MiniDOM/MiniDOM" \
+    --root-url "https://minidom.github.io" \
     --module "MiniDOM" \
     --output "$output_dir"
 
