40a356c6800a43938d6455413f0fec2ee44ab373
Paul Calnan <me@paulcalnan.com>
2018-01-16 10:26:04 -0500

    Fixed build/test target device

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ OSX_SDK="macosx10.12"
 TVOS_SDK="appletvsimulator10.2"
 WATCHOS_SDK="watchsimulator3.2"
 
-IOS_DESTINATION="OS=10.3,name=iPhone SE"
+IOS_DESTINATION="OS=10.3.1,name=iPhone SE"
 MACOS_DESTINATION="arch=x86_64"
 TVOS_DESTINATION="OS=10.2,name=Apple TV 1080p"
 WATCHOS_DESTINATION="OS=3.2,name=Apple Watch - 42mm"
