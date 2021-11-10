7c7cbf234a5558e1dbd4bf387c0022cedbeee144
Paul Calnan <pwc3@users.noreply.github.com>
2021-11-10 13:48:08 -0500

    First cut at resource utilization measurement

--- Sources/PerformanceTest/PerformanceTest.swift
+++ Sources/PerformanceTest/PerformanceTest.swift
@@ -10,95 +10,47 @@ struct PerformanceTest: ParsableCommand {
 }
 
 struct Options: ParsableArguments {
-    @Argument(help: "Input XML files")
-    var inputFiles: [FileArgument]
-}
+    @Argument(help: "Input XML file")
+    var inputFilename: String
 
-struct FileArgument: ExpressibleByArgument, Decodable {
-    var url: URL
-
-    init?(argument: String) {
-        self.url = URL(fileURLWithPath: argument, isDirectory: false)
+    var inputURL: URL {
+        URL(fileURLWithPath: inputFilename)
     }
 }
 
-protocol Subcommand: ParsableCommand {
-    var options: Options { get }
-}
-
-extension Subcommand {
-    var inputURLs: [URL] {
-        options.inputFiles.map { $0.url.absoluteURL }
-    }
-
-    func printIfVerbose(_ string: String) {
-        print(string)
-    }
-
-    func printOptions() {
-        print(options)
-    }
-}
-
-struct DocumentFromURL: Subcommand {
+struct DocumentFromURL: ParsableCommand {
     @OptionGroup
     var options: Options
 
     func run() throws {
-        try measure("document-from-url: all") {
-            for url in inputURLs {
-                try measure("document-from-url: \(url)") {
-                    _ = Document(contentsOf: url)
-                }
-            }
+        try measure("document-from-url: \(options.inputFilename)") {
+            _ = Document(contentsOf: options.inputURL)
         }
     }
 }
 
-struct DocumentFromData: Subcommand {
+struct DocumentFromData: ParsableCommand {
     @OptionGroup
     var options: Options
 
     func run() throws {
-        try measure("document-from-data: all") {
-            for url in inputURLs {
-                try measure("document-from-data: \(url)") {
-                    let data = try Data(contentsOf: url)
-                    _ = Document(data: data)
-                }
-            }
+        try measure("document-from-data: \(options.inputFilename)") {
+            let data = try Data(contentsOf: options.inputURL)
+            _ = Document(data: data)
         }
     }
 }
 
-struct StreamElements: Subcommand {
+struct StreamElements: ParsableCommand {
     @OptionGroup
     var options: Options
 
     func run() throws {
-        try measure("stream-elements: all") {
-            for url in inputURLs {
-                try measure("stream-elements: \(url)") {
-                    SAXParser(contentsOf: url)?.streamElements(to: { _ in true }, filter: { _ in true })
-                }
-            }
+        try measure("stream-elements: \(options.inputFilename)") {
+            SAXParser(contentsOf: options.inputURL)?.streamElements(
+                to: { _ in true },
+                filter: { _ in true }
+            )
         }
     }
-}
-
-func measure(_ description: String, worker: () throws -> Void) throws {
-    let start = clock()
-    defer {
-        let end = clock()
-        let delta = timeDelta(start, end)
-        print("\(description) completed in \(delta) seconds")
-    }
-
-    print("Starting \(description)")
-    try worker()
-}
-
-func timeDelta(_ t1: clock_t, _ t2: clock_t) -> TimeInterval {
-    let delta = Int64(t2) - Int64(t1)
-    return TimeInterval(delta) / TimeInterval(CLOCKS_PER_SEC)
 }
